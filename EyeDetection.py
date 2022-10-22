36300688e2487eb29c8939e1b6513dc2b4d9b49d
anugoyal998 <anugoyal998@gmail.com>
2022-10-22 16:43:01 +0530

    done some minor changes

--- EyeDetection.py
+++ EyeDetection.py
@@ -16,10 +16,18 @@ import mediapipe as mp
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh()
 
-leftEyeLmIndex = [263, 249, 390, 373, 374, 380, 381, 382, 362]
-rightEyeLmIndex = [33, 7, 163, 144, 145, 153, 154, 155, 133]
+leftEyeLmIndex = [263, 249, 390, 373, 374, 380, 381, 382, 362] # indexs of left eye landmarks
+rightEyeLmIndex = [33, 7, 163, 144, 145, 153, 154, 155, 133] # indexs of right eye landmarks
 
 def appendAndDrawEyesLandmarks(lmIndex,faceLandmarks,img,eyesLandmarks,draw):
+    """
+    Args:
+        lmIndex -> list of landmark indexs \n
+        faceLandmarks -> mediapipe faceLandmarks \n
+        img -> numpy image \n
+        eyesLandmarks -> list to append coordinates \n
+        draw -> bool to draw on ROI
+    """
     imgHeight, imgWidth, imgChannel = img.shape # extract image Height, and width
     for index in lmIndex:
         landmark = faceLandmarks.landmark[index]
@@ -31,6 +39,14 @@ def appendAndDrawEyesLandmarks(lmIndex,faceLandmarks,img,eyesLandmarks,draw):
             cv2.circle(img,(x_coord,y_coord),1,(255,0,0),2)
 
 def detectEyes(img,show=False):
+    """
+    Args:
+        img -> numpy image \n
+        show -> bool to draw on ROI
+    returns:
+        number of ROIs detected
+    """
+    
     # convert BGR image to RGB image
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # process image
@@ -44,14 +60,14 @@ def detectEyes(img,show=False):
             appendAndDrawEyesLandmarks(leftEyeLmIndex,faceLandmarks,img,eyesLandmarks,show)
             appendAndDrawEyesLandmarks(rightEyeLmIndex,faceLandmarks,img,eyesLandmarks,show)
     
-    size = len(leftEyeLmIndex) + len(rightEyeLmIndex)
+    total_landmarks = len(leftEyeLmIndex) + len(rightEyeLmIndex)
     eyesLandmarksLength = len(eyesLandmarks)
     
     cv2.imshow('img',img)
     
-    if eyesLandmarksLength < size/2:
+    if eyesLandmarksLength < total_landmarks/2:
         return 0
-    elif eyesLandmarksLength >= size/2 and eyesLandmarksLength < size:
+    elif eyesLandmarksLength >= total_landmarks/2 and eyesLandmarksLength < total_landmarks:
         return 1
-    elif eyesLandmarksLength >= size:
+    elif eyesLandmarksLength == total_landmarks:
         return 2
